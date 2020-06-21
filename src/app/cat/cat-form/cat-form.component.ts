c0e1ddd2c80a21be76b486fbe83535a3efcab2a1
Thamiris Mylius Monteiro <thamirismylius@gmail.com>
2020-06-21 19:01:01 +0000

    cute catsies

--- src/app/cat/cat-form/cat-form.component.ts
+++ src/app/cat/cat-form/cat-form.component.ts
@@ -6,12 +6,13 @@ import {ActivatedRoute, Router} from '@angular/router';
 @Component({
   selector: 'app-cat-form',
   templateUrl: './cat-form.component.html',
-  styleUrls: ['./cat-form.component.scss']
+  styleUrls: ['./cat-form.component.css']
 })
 export class CatFormComponent implements OnInit {
 
   public cat: Cat = new Cat();
   public displayMsg: boolean;
+  public displayMsg2: boolean;
 
   constructor(private catService: CatService, private router: Router, private activatedRoute: ActivatedRoute) {
     if (this.activatedRoute.snapshot.params.id != null){
@@ -27,11 +28,34 @@ export class CatFormComponent implements OnInit {
   }
 
   save(): void{
-    if (this.cat._id != null){
-      this.update();
-    }else{
-      this.insert();
+    if (!this.cat.name){
+      alert("Please inform the cat's name");
     }
+    else {
+      if (!this.cat.age){
+        alert("Please inform the cat's age");
+      }
+      else{
+        if (!this.cat.breed){
+          alert("Please inform the cat's breed");
+        }
+        else {
+          if (!this.cat.favoriteToy){
+            alert("Please inform the cat's favorite toy");
+          }
+          else {
+            if (this.cat._id != null){
+              this.update();
+            }else{
+              this.insert();
+            }
+          }
+        }
+      }
+    }
+  }
+  goBack(): void{
+    window.history.back();
   }
 
   private update(): void{
@@ -48,7 +72,13 @@ export class CatFormComponent implements OnInit {
   private insert(): void{
     this.catService.addCat(this.cat)
       .then((cat: Cat) => {
-        this.router.navigate(['/cats']);
+        this.displayMsg2 = true;
+        setTimeout(() => {
+          this.displayMsg2 = false;
+        }, 3000);
+        setTimeout(() => {
+          this.router.navigate(['/cats']);
+        },500);
       })
       .catch(e => console.error(e));
   }
--- src/app/cat/cat-list/cat-list.component.ts
+++ src/app/cat/cat-list/cat-list.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {CatService} from '../../services/cat.service';
-import {Cat} from '../../model/cat.model';
+import { CatService } from '../../services/cat.service';
+import { Cat } from '../../model/cat.model';
 
 @Component({
   selector: 'app-cat-list',
   templateUrl: './cat-list.component.html',
-  styleUrls: ['./cat-list.component.css']
+  styleUrls: ['./cat-list.component.css'],
 })
 export class CatListComponent implements OnInit {
-
   public catList: Array<Cat>;
+  public deleteCat: Boolean;
+  public displayMsg: boolean;
 
   constructor(private catService: CatService) {
     this.catService.catDeletedEvent.subscribe((deletedCat: Cat) => {
       this.catList = this.catList.filter((cat: Cat) => {
-        if (cat._id === deletedCat._id){
+        if (cat._id === deletedCat._id) {
           return false;
-        }else{
+        } else {
           return true;
         }
       });
@@ -24,19 +25,33 @@ export class CatListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.catService.listCats()
+    this.catService
+      .listCats()
       .then((catListFromDb: Array<Cat>) => {
         this.catList = catListFromDb;
       })
-      .catch(e => console.error(e));
+      .catch((e) => console.error(e));
   }
 
   delete(cat: Cat): void {
+    var r = confirm('Are you sure you want to delete ' + cat.name + '?');
+    if (r == true) {
+      this.deleteCat = true;
+    } else {
+      this.deleteCat = false;
+    }
+    if (this.deleteCat)
+    {
     this.catService.deleteCat(cat._id)
       .then(() => {
         this.catService.catDeletedEvent.emit(cat);
       })
-      .catch(e => console.error(e));
+      .catch((e) => console.error(e));
+    }
+    this.displayMsg = true;
+        setTimeout(() => {
+          this.displayMsg = false;
+        }, 3000);
   }
-
 }
+
